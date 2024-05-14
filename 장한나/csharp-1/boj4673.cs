18baf14eaed875ce346591262dd2033594f84da2
Hanna.Jang <wkdgkssk0711@gmail.com>
2024-05-14 19:32:09 +0900

    장한나 csharp 1차 과제

---
+++ 장한나/csharp-1/boj4673.cs
@@ -0,0 +1,37 @@
+﻿//백준 실버 5
+//4673. 셀프 넘버
+
+using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 10001; i++)
+        {
+            if (RevD(i) == -1) Console.WriteLine(i);
+        }
+    }
+
+    public static int D(int n)
+    {
+        int sum = n;
+        while (n != 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    public static int RevD(int dn)
+    {
+        for (int i = 0; i < 10001; i++)
+        {
+            if (D(i) == dn) return i;
+        }
+
+        return -1;
+    }
+}
