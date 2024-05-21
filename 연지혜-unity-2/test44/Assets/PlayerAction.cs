ef6ea8e02c6df16028bf3311e1544f48dca0ddcf
Jihye <a09057@naver.com>
2024-05-21 13:31:56 +0900

    Unity_session2

---
+++ 연지혜-unity-2/test44/Assets/PlayerAction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAction : MonoBehaviour
+{
+    //testtest
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+}
